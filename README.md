e6b7c0be62f816f26ef18d29f9edf69a032463ed
Damjan 9000 <damjan.9000@gmail.com>
2023-11-18 00:58:27 +0100

    support also LSP and Treesitter

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Show Highlight for Neovim
 
-Show regular vim syntax highlight group under cursor in lualine status line
+Show LSP or Treesitter or regular vim syntax highlight group under cursor in lualine status line
 
 default keymap to enable the functionality is: `<leader>th`
 
 to disable the default keymap use the following parameter to setup():
 `{ disable_keymap = true, }`
-
-Treesitter highlight will be disabled when show highlight is enabled
 
 ## Installation
 
--- lua/show-highlight.lua
+++ lua/show-highlight.lua
@@ -2,26 +2,15 @@
 -- default keymap to enable the functionality is: <leader>th
 -- to disable the default keymap add to lazy plugin spec:
 -- opts = { disable_keymap = true, }
--- Treesitter highlight will be disabled when show highlight is enabled
--- (with: :TSDisable highlight all)
 
---[[
--- more details:
-function SynStack2()
-  for _, i1 in ipairs(vim.fn.synstack(vim.fn.line('.'), vim.fn.col('.'))) do
-    local i2 = vim.fn.synIDtrans(i1)
-    local n1 = vim.fn.synIDattr(i1, 'name')
-    local n2 = vim.fn.synIDattr(i2, 'name')
-    print(n1, '->', n2)
-  end
-end
---]]
 
 vim.g.highlight_line_active = false
 vim.g.highlight_lualine_enabled = false
 
 local M = {}
 
+--[[
+--OLD
 function M.SynStackStr()
   local s = ""
   for _, i1 in ipairs(vim.fn.synstack(vim.fn.line('.'), vim.fn.col('.'))) do
@@ -35,10 +24,68 @@ function M.SynStackStr()
   s = "[" .. s .. "]"
   return s
 end
+--]]
+
+function M.fmt_hl_group(s, hdr, o)
+  if o then
+    if o.hl_group == o.hl_group_link then
+      s = string.format("%s%s%s ", s, hdr, o.hl_group)
+    else
+      s = string.format("%s%s%s->%s ", s, hdr, o.hl_group, o.hl_group_link)
+    end
+  end
+  return s
+end
+
+function M.has_color(hl)
+  local synid = vim.fn.synIDtrans(vim.fn.hlID(hl))
+  local bg = vim.fn.synIDattr(synid, 'bg')
+  local fg = vim.fn.synIDattr(synid, 'fg')
+  if (fg and fg ~= '') or (bg and bg ~= '') then
+    return true
+  else
+    return false
+  end
+end
+
+function M.last_prio_with_color(syntable)
+  local o = nil
+  -- get highest priority with a defined fg or bg
+  for _, x in ipairs(syntable) do
+    local xo = x
+    if x.opts then xo = x.opts end
+    if not o then
+      o = xo
+    else
+      local prio = true
+      if o.priority and xo.priority then prio = xo.priority > o.priority end
+      if prio and M.has_color(xo.hl_group_link) then
+        o = xo
+      end
+    end
+  end
+  return o
+end
+
+function M.InspectSynStr()
+  local s = "[ "
+  local o = nil
+  local insp = vim.inspect_pos()
+  if insp then
+    o = M.last_prio_with_color(insp.syntax)
+    s = M.fmt_hl_group(s, "syn:", o)
+    o = M.last_prio_with_color(insp.treesitter)
+    s = M.fmt_hl_group(s, "ts:", o)
+    o = M.last_prio_with_color(insp.semantic_tokens)
+    s = M.fmt_hl_group(s, "lsp:", o)
+  end
+  s = s .. "]"
+  return s
+end
 
 function M.SynStackLine()
   if vim.g.highlight_line_active then
-    return M.SynStackStr()
+    return M.InspectSynStr()
   else
     return ""
   end
@@ -47,6 +94,7 @@ end
 function M.SynStackToggle()
   if not vim.g.highlight_lualine_enabled then
     if pcall(require, 'lualine') then
+        -- add an entry in lualine
       local ll_x = require('lualine').get_config().sections.lualine_x
       table.insert(ll_x, 1, M.SynStackLine)
       require('lualine').setup({
@@ -56,10 +104,6 @@ function M.SynStackToggle()
       })
     end
     vim.g.highlight_lualine_enabled = true
-    -- disable treesitter highlight if treesitter present
-    if pcall(require, 'nvim-treesitter') then
-      vim.cmd(':TSDisable highlight all')
-    end
   end
   vim.g.highlight_line_active = not vim.g.highlight_line_active
 end
@@ -70,8 +114,6 @@ function M.setup(opts)
   if not disable_keymap then
     vim.keymap.set('n', '<leader>th', M.SynStackToggle,
       {
-        noremap = true,
-        silent = true,
         desc = '[T]oggle [H]ighlight under cursor'
       })
   end
